17b0316e79b4cf113664718455484f280651fb5b
moita69 <fmj1988@gmail.com>
2016-07-17 18:08:43 -0300

    speedcomplainer works

--- speedcomplainer.py
+++ speedcomplainer.py
@@ -9,7 +9,8 @@ import twitter
 import json 
 import random
 from logger import Logger
-
+import oauth as OAuth
+from twython import Twython
 shutdownFlag = False
 
 def main(filename, argv):
@@ -25,19 +26,22 @@ def main(filename, argv):
 
     while not shutdownFlag:
         try:
-
+            print("runing monitor time: "+str(datetime.now()))
             monitor.run()
-
-            for i in range(0, 5):
+            #print("runed monitor time: ",datetime.now())
+            for i in range(0, 6):
                 if shutdownFlag:
                     break
-                time.sleep(1)
+                time.sleep(10)
+                #print("click sleep time: ",datetime.now())
 
         except Exception as e:
-            print 'Error: %s' % e
+            print 'Error01: %s' % e
             sys.exit(1)
 
     sys.exit()
+
+
 
 def shutdownHandler(signo, stack_frame):
     global shutdownFlag
@@ -48,15 +52,36 @@ class Monitor():
     def __init__(self):
         self.lastPingCheck = None
         self.lastSpeedTest = None
+        #testing twitter  connection purpose
+        #self.twitterAuth()
 
+    def twitterAuth(self):
+        
+        print ("twitter api initialization")
+        self.config = json.load(open('./config.json'))
+        TOKEN=self.config['twitter']['twitterTokenSecret']
+        CON_SEC_KEY=self.config['twitter']['twitterConsumerKey']
+        CON_SEC=self.config['twitter']['twitterConsumerSecret']
+        TOKEN_KEY=self.config['twitter']['twitterToken']
+        twitter = Twython(CON_SEC_KEY, CON_SEC,TOKEN_KEY, TOKEN)
+        twitter.update_status(status='See how easy using Twython is!')
+        #my_auth = twitter.OAuth(TOKEN,TOKEN_KEY,CON_SEC,CON_SEC_KEY)
+        #twit = twitter.Twitter(auth=my_auth)
+        #twit.statuses.update(status="I'm tweeting from Python!")
+        #api = twitter.api.Twitter(auth=OAuth(consumer_key=tck,consumer_secret=tcs,access_token_key=ttk,access_token_secret=tts))
+        print ("twitter api initialized")
+    
     def run(self):
         if not self.lastPingCheck or (datetime.now() - self.lastPingCheck).total_seconds() >= 60:
             self.runPingTest()
             self.lastPingCheck = datetime.now()
+            print("ping test executed")
 
-        if not self.lastSpeedTest or (datetime.now() - self.lastSpeedTest).total_seconds() >= 3600:
+        #if not self.lastSpeedTest or (datetime.now() - self.lastSpeedTest).total_seconds() >= 3600:
+        if not self.lastSpeedTest or (datetime.now() - self.lastSpeedTest).total_seconds() >= 300:
             self.runSpeedTest()
             self.lastSpeedTest = datetime.now()
+            print("Speed test executed")
 
     def runPingTest(self):
         pingThread = PingTest()
@@ -84,6 +109,7 @@ class PingTest(threading.Thread):
         success = 0
         if response == 0:
             success = 1
+        print("success:"+str(success))
         return { 'date': datetime.now(), 'success': success }
 
     def logPingResults(self, pingResults):
@@ -101,16 +127,18 @@ class SpeedTest(threading.Thread):
         self.tweetResults(speedTestResults)
 
     def doSpeedTest(self):
+        print "Starting speedtest"
         # run a speed test
         result = os.popen("/usr/local/bin/speedtest-cli --simple").read()
         if 'Cannot' in result:
             return { 'date': datetime.now(), 'uploadResult': 0, 'downloadResult': 0, 'ping': 0 }
-
+        print "result: "+str(result)
         # Result:
         # Ping: 529.084 ms
         # Download: 0.52 Mbit/s
         # Upload: 1.79 Mbit/s
-
+        #import pdb; pdb.set_trace()
+        
         resultSet = result.split('\n')
         pingResult = resultSet[0]
         downloadResult = resultSet[1]
@@ -120,10 +148,12 @@ class SpeedTest(threading.Thread):
         downloadResult = float(downloadResult.replace('Download: ', '').replace(' Mbit/s', ''))
         uploadResult = float(uploadResult.replace('Upload: ', '').replace(' Mbit/s', ''))
 
+        print ("Done speedtest")
         return { 'date': datetime.now(), 'uploadResult': uploadResult, 'downloadResult': downloadResult, 'ping': pingResult }
 
     def logSpeedTestResults(self, speedTestResults):
         self.logger.log([ speedTestResults['date'].strftime('%Y-%m-%d %H:%M:%S'), str(speedTestResults['uploadResult']), str(speedTestResults['downloadResult']), str(speedTestResults['ping']) ])
+        print("Done log")
 
 
     def tweetResults(self, speedTestResults):
@@ -132,15 +162,35 @@ class SpeedTest(threading.Thread):
         for (threshold, messages) in thresholdMessages.items():
             threshold = float(threshold)
             if speedTestResults['downloadResult'] < threshold:
-                message = messages[random.randint(0, len(messages) - 1)].replace('{tweetTo}', self.config['tweetTo']).replace('{internetSpeed}', self.config['internetSpeed']).replace('{downloadResult}', str(speedTestResults['downloadResult']))
-
+                    #message = messages[random.randint(0, len(messages) - 1)].replace('{tweetTo}', self.config['tweetTo']).replace('{internetSpeed}', self.config['internetSpeed']).replace('{downloadResult}', str(speedTestResults['downloadResult']))
+                    message = messages[0].replace('{tweetTo}', self.config['tweetTo']).replace('{internetSpeed}', self.config['internetSpeed']).replace('{downloadResult}', str(speedTestResults['downloadResult']))
+                    print ("message:",message)
         if message:
-            api = twitter.api(consumer_key=self.config['twitter']['twitterConsumerKey'],
-                            consumer_secret=self.config['twitter']['twitterConsumerSecret'],
-                            access_token_key=self.config['twitter']['twitterToken'],
-                            access_token_secret=self.config['twitter']['twitterTokenSecret'])
+            #api = twitter.api(consumer_key=self.config['twitter']['twitterConsumerKey'],
+            #                consumer_secret=self.config['twitter']['twitterConsumerSecret'],
+            #                access_token_key=self.config['twitter']['twitterToken'],
+            #                access_token_secret=self.config['twitter']['twitterTokenSecret'])
+            #TOKEN_KEY=self.config['twitter']['twitterTokenSecret']
+            #CON_SEC_KEY=self.config['twitter']['twitterConsumerKey']
+            #CON_SEC=self.config['twitter']['twitterConsumerSecret']
+            #TOKEN=self.config['twitter']['twitterToken']
+            #my_auth = twitter.OAuth(TOKEN,TOKEN_KEY,CON_SEC,CON_SEC_KEY)
+            #twit = twitter.Twitter(auth=my_auth)
+            TOKEN=self.config['twitter']['twitterTokenSecret']
+            CON_SEC_KEY=self.config['twitter']['twitterConsumerKey']
+            CON_SEC=self.config['twitter']['twitterConsumerSecret']
+            TOKEN_KEY=self.config['twitter']['twitterToken']
+            api = Twython(CON_SEC_KEY, CON_SEC,TOKEN_KEY, TOKEN)
+            
             if api:
-                status = api.PostUpdate(message)
+                #status = api.PostUpdate(message)
+                #twit.statuses.update(status=message)
+                api.update_status(status=message)
+            else:
+                print ("No API")
+        else:
+            print ("No message")
+        print("Done tweetResult")
 
 class DaemonApp():
     def __init__(self, pidFilePath, stdout_path='/dev/null', stderr_path='/dev/null'):
